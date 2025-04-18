21d233d447a90db91183dd7eaacb545cccad8e2d
Naseer-010 <naseer20704446@gmail.com>
2025-04-18 07:12:48 +0530

    Template updates

--- ContextAwarebot/ContextAwarebot/views.py
+++ ContextAwarebot/ContextAwarebot/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from aiengine.ml.main import Result, Model
 
 def home(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        prompt=request.POST.get('prompt')
+        Domain= Result(prompt)
+        response = Model(prompt, Domain)
+        return render(request, 'index.html', {'response': 'response'})
+    else:
+        return render(request, 'index.html')
--- ContextAwarebot/aiengine/ml/main.py
+++ ContextAwarebot/aiengine/ml/main.py
@@ -1,4 +1,75 @@
-from Classifier import Classifier
-from ragbio import bio
+from . import classifier
 from dotenv import load_dotenv
-from ragphy import phy 
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq 
+
+load_dotenv()
+
+
+prompt = input()
+
+def Result(prompt):
+    domain = classifier(prompt)
+    return domain
+
+def Model(prompt, domain):
+
+    if(domain == "medical"):
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
+
+    elif(domain == "engineering"):
+
+        llm = ChatGroq(model="llama-3.3-70b-versatile")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "finance"):
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "legal"):
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "technology"):
+
+        llm = ChatGroq(model="llama-3.3-70b-versatile")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "business"):
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
+            
+    elif(domain == "coding"):
+
+        llm = ChatGroq(model="llama-3.3-70b-versatile")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "entertainment"):
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    elif(domain == "environment sciences"):
+
+        llm = ChatGroq(model="llama-3.3-70b-versatile")
+        response = llm.invoke(prompt)
+        return response.content
+    
+    else:
+
+        llm = ChatGoogleGenerativeAI(model="gemini-2.5-pro-exp-03-25")
+        response = llm.invoke(prompt)
+        return response.content
